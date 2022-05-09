456e0a3b95256602c0d16a77f236c7a3964ce125
gbarron2014 <gbarron@utng.edu.mx>
2022-05-09 16:56:11 -0500

    9 mayo

--- src/main/java/com/sic/App.java
+++ src/main/java/com/sic/App.java
@@ -1,13 +1,15 @@
 package com.sic;
 
 /**
- * Hello world!
- *
+ * Programa Inicial con el lenguaje de Programaciòn Java!
+ * Autor:  Gabriel Barròn Rodriguez
+ * Version: 1.0
  */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World Iot!" );
+        System.out.println("Autor Gabriel Barron");
     }
 }
